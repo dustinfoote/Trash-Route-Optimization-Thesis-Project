050cc0d1bf9dfc40caf65b221fb61f177cdb8ddc
dustinfoote <60949055+dustinfoote@users.noreply.github.com>
2020-03-17 12:44:10 -0700

    Updated ILP
    Fixed idxs vector and proper distance data now being used

--- updated_intlinprog_MATLAB_solver.m
+++ updated_intlinprog_MATLAB_solver.m
@@ -2,20 +2,34 @@ clear;clc;
 m=1; %number of salemen/vehicles
 DATA=load('Final Distance Matrix.mat');
 DATA=DATA.DATA;
+DATACopy=DATA;
 [Longitude, Latitude] = readvars('Dual Litter Bins_Tempe_LatLong_Distance Matrix with Compactors +Depot.xlsx','Sheet','Sheet2','Range','B5:C243');
 
 %LINE BELOW IS NEW
-DATA=DATA([1,70:83],[1,70:83]);
-%Longitude=Longitude(1:17);
-%Latitude=Latitude(1:17);
+datanodes=[1,56,100,13,231]; % row vector, must always start with node 1
+%DATA=DATA([1,5,76,108,223],[1,5,76,108,223]);
+Long=Longitude(datanodes);
+Lat=Latitude(datanodes);
 
-nStops = length(DATA); % you can use any number, but the problem size scales as N^2
+nStops = length(datanodes); % you can use any number, but the problem size scales as N^2
 nDLs=nStops-1;
 
 
 %%% Calculate Distances Between Points
-idxs = nchoosek(1:nStops,2);
-idxs=[idxs;idxs(:,2),idxs(:,1)]; 
+idxs1 = nchoosek(1:238,2);
+idxs1=[idxs1;idxs1(:,2),idxs1(:,1)]; 
+idxs=[];
+id=[];
+for p=1:nStops
+    for w=1:nStops
+id=find(idxs1(:,1)==datanodes(p) & idxs1(:,2)==datanodes(w));
+idxs=[idxs;idxs1(id,:)];
+%id=find(idxs1(:,2)==datanodes(p) & idxs1(:,1)==datanodes(w)); 
+%idxs=[idxs;idxs1(id,:)];
+    end
+end
+
+
 nCombs=length(idxs);
 
 dist=zeros(nCombs,1);
@@ -39,7 +53,7 @@ beq(2)=m;
 %% Constraint 4
 
 for j=2:nStops
-    rowidxs=find(idxs(:,2)==j);
+    rowidxs=find(idxs(:,2)==datanodes(j));
     Aeq(j+1,rowidxs)=ones(1,nDLs);
     beq(j+1)=1;
 end
@@ -47,7 +61,7 @@ end
 %% Constraint 5
 cnt=nStops+2;
 for i=2:nStops
-    rowidxs=find(idxs(:,1)==i);
+    rowidxs=find(idxs(:,1)==datanodes(i));
     Aeq(cnt,rowidxs)=ones(1,nDLs);
     beq(cnt)=1;
     cnt=cnt+1;
@@ -56,12 +70,12 @@ end
 %% Constraint 6, 7
 A=zeros(2*nDLs+nDLs^2,nCombs+nDLs);
 b=zeros(2*nDLs+nDLs^2,1);
-L=14;
-K=4;
+L=10;
+K=1;
 cnter=nStops;
 for i=2:nStops
-    x1i=find(idxs(:,1)==1 & idxs(:,2)==i);
-    xi1=find(idxs(:,2)==1 & idxs(:,1)==i);
+    x1i=find(idxs(:,1)==1 & idxs(:,2)==datanodes(i));
+    xi1=find(idxs(:,2)==1 & idxs(:,1)==datanodes(i));
     ui=nCombs+i-1;
     A(i-1,[x1i,xi1,ui])=[L-2,-1,1];
     b(i-1)=L-1;
@@ -77,8 +91,8 @@ for i=2:nStops
         if i==j
             % do nothing
         else
-            xij=find(idxs(:,1)==i & idxs(:,2)==j);
-            xji=find(idxs(:,2)==i & idxs(:,1)==j);
+            xij=find(idxs(:,1)==datanodes(i) & idxs(:,2)==datanodes(j));
+            xji=find(idxs(:,2)==datanodes(i) & idxs(:,1)==datanodes(j));
             ui=nCombs+i-1;
             uj=nCombs+j-1;
             A(counter,[xij,xji,ui,uj])=[L,L-2,1,-1]; % On March 11 changed from -2 to -1 for u_j
@@ -115,47 +129,49 @@ truetripscopy=truetrips;
 xplot=zeros(1,2);
 yplot=zeros(1,2);
 %%
-% figure;hold on;
-% for i=1:length(truetripscopy)
-%     xplot=[Latitude(truetripscopy(i,1)),Latitude(truetripscopy(i,2))];
-%     yplot=[Longitude(truetripscopy(i,1)),Longitude(truetripscopy(i,2))];
-%     plot(yplot,xplot,'b')
-% end
-% plot(Longitude,Latitude,'r*')
-% hold off
+figure;hold on;
+plot(Longitude,Latitude,'r*')
+plot(Long,Lat,'g*')
+for i=1:length(truetripscopy)
+    xplot=[Latitude(truetripscopy(i,1)),Latitude(truetripscopy(i,2))];
+    yplot=[Longitude(truetripscopy(i,1)),Longitude(truetripscopy(i,2))];
+    plot(yplot,xplot,'b')
+end
+
+hold off
 
 
 %%
-figure;hold on;
-for h=1:m
-q=1;
-yplot(2)=0;
-while yplot(2)~=Longitude(1)
-    if q==1
-        i=find(truetrips(:,1)==1);
-        i=i(1);
-    else
-        i=find(truetrips(:,1)==nextDL);
-        i=i(1);
-    end
-    q=2;
-    xplot=[Latitude(truetrips(i,1)),Latitude(truetrips(i,2))];
-    yplot=[Longitude(truetrips(i,1)),Longitude(truetrips(i,2))];
-    if h==1
-        plot(yplot,xplot,'b')    
-    elseif h==2
-        plot(yplot,xplot,'c')
-    elseif h==3
-        plot(yplot,xplot,'g')
-    elseif h==4
-        plot(yplot,xplot,'m')  
-    elseif h==5
-        plot(yplot,xplot,'k')  
-    end
-    nextDL=truetrips(i,2);
-    truetrips(i,:)=[];
-    
-end
-end
-plot(Longitude,Latitude,'r*')
-hold off
+% figure;hold on;
+% for h=1:m
+% q=1;
+% yplot(2)=0;
+% while yplot(2)~=Longitude(1)
+%     if q==1
+%         i=find(truetrips(:,1)==1);
+%         i=i(1);
+%     else
+%         i=find(truetrips(:,1)==nextDL);
+%         i=i(1);
+%     end
+%     q=2;
+%     xplot=[Latitude(truetrips(i,1)),Latitude(truetrips(i,2))];
+%     yplot=[Longitude(truetrips(i,1)),Longitude(truetrips(i,2))];
+%     if h==1
+%         plot(yplot,xplot,'b')    
+%     elseif h==2
+%         plot(yplot,xplot,'c')
+%     elseif h==3
+%         plot(yplot,xplot,'g')
+%     elseif h==4
+%         plot(yplot,xplot,'m')  
+%     elseif h==5
+%         plot(yplot,xplot,'k')  
+%     end
+%     nextDL=truetrips(i,2);
+%     truetrips(i,:)=[];
+%     
+% end
+% end
+% plot(Longitude,Latitude,'r*')
+% hold off
